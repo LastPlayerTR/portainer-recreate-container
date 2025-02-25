9e70758384510e792f219165ea2a73d8b1206227
MetaPC <omertezcan141@gmail.com>
2025-02-26 00:20:34 +0300

    sa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
-
+import fetch from 'node-fetch'
 /**
  * The main function for the action.
  *
@@ -8,18 +7,69 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const portainerHost: string = core.getInput('portainer-host', {
+      required: true
+    })
+    const username: string = core.getInput('username', {
+      required: true
+    })
+    const password: string = core.getInput('password', {
+      required: true
+    })
+    const containerName: string = core.getInput('containerName', {
+      required: true
+    })
+    core.setSecret(password)
+    core.info(`Portainer Host: ${portainerHost}`)
+    const authResponse = await fetch(`${portainerHost}/api/auth`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    if(!authResponse.ok) {
+      throw new Error(`Failed to authenticate with Portainer: ${authResponse.statusText}`)
+    }
+    const authData: any = await authResponse.json()
+    const token = authData.jwt
+    core.setSecret(token)
+    const containersResponse = await fetch(`${portainerHost}/api/endpoints/3/docker/v1.41/containers/json?all=true`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    if(!containersResponse.ok) {
+      throw new Error(`Failed to get containers from Portainer: ${containersResponse.statusText}`)
+    }
+    const containersData: any = await containersResponse.json()
+    const container = containersData.find((container: any) => container.Names.includes(`/${containerName}`))
+    if(!container) {
+      throw new Error(`Container not found: ${containerName}`)
+    }
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const containerId = container.Id
+    core.info(`Container ID: ${containerId}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const recreateResponse = await fetch(`${portainerHost}/api/endpoints/3/docker/containers/${containerId}/recreate`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if(!recreateResponse.ok) {
+      throw new Error(`Failed to recreate container: ${recreateResponse.statusText}`)
+    }
+
+    core.info(`Recreated container: ${containerName}`)
+
+
+
+
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
