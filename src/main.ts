095082a5347df3f6444174d17b5c7e0b8e1184a1
MetaPC <omertezcan141@gmail.com>
2025-02-26 16:55:20 +0300

    bizziko

--- src/main.ts
+++ src/main.ts
@@ -31,46 +31,56 @@ export async function run(): Promise<void> {
         password
       })
     })
-    if(!authResponse.ok) {
-      throw new Error(`Failed to authenticate with Portainer: ${authResponse.statusText}`)
+    if (!authResponse.ok) {
+      throw new Error(
+        `Failed to authenticate with Portainer: ${authResponse.statusText}`
+      )
     }
     const authData: any = await authResponse.json()
     const token = authData.jwt
     core.setSecret(token)
-    const containersResponse = await fetch(`${portainerHost}/api/endpoints/3/docker/v1.41/containers/json?all=true`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
+    const containersResponse = await fetch(
+      `${portainerHost}/api/endpoints/3/docker/v1.41/containers/json?all=true`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
       }
-    })
-    if(!containersResponse.ok) {
-      throw new Error(`Failed to get containers from Portainer: ${containersResponse.statusText}`)
+    )
+    if (!containersResponse.ok) {
+      throw new Error(
+        `Failed to get containers from Portainer: ${containersResponse.statusText}`
+      )
     }
     const containersData: any = await containersResponse.json()
-    const container = containersData.find((container: any) => container.Names.includes(`/${containerName}`))
-    if(!container) {
+    const container = containersData.find((container: any) =>
+      container.Names.includes(`/${containerName}`)
+    )
+    if (!container) {
       throw new Error(`Container not found: ${containerName}`)
     }
 
     const containerId = container.Id
     core.info(`Container ID: ${containerId}`)
 
-    const recreateResponse = await fetch(`${portainerHost}/api/endpoints/3/docker/containers/${containerId}/recreate`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify({"PullImage":true})
-    })
+    const recreateResponse = await fetch(
+      `${portainerHost}/api/endpoints/3/docker/containers/${containerId}/recreate`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ PullImage: true })
+      }
+    )
 
-    if(!recreateResponse.ok) {
-      throw new Error(`Failed to recreate container: ${recreateResponse.statusText}`)
+    if (!recreateResponse.ok) {
+      throw new Error(
+        `Failed to recreate container: ${recreateResponse.statusText}`
+      )
     }
 
     core.info(`Recreated container: ${containerName}`)
-
-
-
-
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
